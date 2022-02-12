872dfa665f828c153e8fee862ecc44678ddc79da
climax-solution <blocklimax@gmail.com>
2022-02-12 18:15:49 +0200

    add validate form

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,15 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
   return (
     <div className="App">
-        <div className="banner"></div>
-
-        <div className="row">
-          <div className="col-sm-6 col-12">
-
+      <div className="container">
+        <div className="row mt-150">
+          <div className="col-sm-4 col-12">
+            <div className="detail-box border border-dark h-100 rounded">
+              <div className="form-group">
+                <label for="token-name"></label>
+                <input type="text" className="form-control" name="token-name"/>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
